a14fb4751f7fea80d8e06d46d80fa7c275d3a613
MarcBehnke <marc.behnke@gmail.com>
2017-02-12 10:10:35 -0600

    Added ActivitySet test.

---
+++ src/ActivitySet/ActivitySet.Test/ActivitySetTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ActivitySet.Test
+{
+
+    [TestFixture]
+    public class ActivitySetTest
+    {
+        [Test]
+        public void CanCreateActivitySet()
+        {
+            //var activitySet = new ActivitySet();
+            Assert.Fail("Rename projects so we don't have a namespace named ActivitySet allowing a class to be named ActivitySet.");
+        }
+    }
+}
