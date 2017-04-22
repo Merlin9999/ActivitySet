e8ce5b0942677307aec3778cff971937348275bf
MarcBehnke <marc.behnke@gmail.com>
2017-04-22 10:04:55 -0500

    Renamed abstract root Lite DB type for clarity.

---
+++ src/PCLActivitySet/PCLActivitySet/Db/AbstractLiteDbObject.cs
@@ -0,0 +1,17 @@
+﻿using LiteDB;
+
+namespace PCLActivitySet.Db
+{
+    public abstract class AbstractLiteDbObject
+    {
+    }
+
+    public abstract class AbstractLiteDbValue : AbstractLiteDbObject
+    {
+    }
+
+    public abstract class AbstractLiteDbEntity : AbstractLiteDbObject
+    {
+        public ObjectId Id { get; set; }
+    }
+}
