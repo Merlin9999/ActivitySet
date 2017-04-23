a69e667e88149a0ae81b5c8c6ee232bfe978789a
MarcBehnke <marc.behnke@gmail.com>
2017-04-23 07:53:37 -0500

    Added ActivityBoardDto.

---
+++ src/PCLActivitySet/PCLActivitySet.Test/Dto/ActivityBoardTest.cs
@@ -0,0 +1,46 @@
+﻿using LiteDB;
+using NUnit.Framework;
+using PCLActivitySet.Domain.Recurrence;
+using PCLActivitySet.Dto.Recurrence;
+using Ploeh.AutoFixture;
+using System.IO;
+using FluentAssertions;
+using PCLActivitySet.Domain;
+using PCLActivitySet.Dto;
+using PCLActivitySet.Test.Helpers;
+
+namespace PCLActivitySet.Test.Dto
+{
+    [TestFixture]
+    public class ActivityBoardTest
+    {
+        [Test]
+        public void DtoAndDomainRoundTrip()
+        {
+            Fixture fixture = TestHelper.CreateSerializationAutoFixture();
+
+            ActivityBoardDto sourceDto = fixture.Create<ActivityBoardDto>();
+            ActivityBoard domain = ActivityBoard.FromDto(sourceDto);
+            ActivityBoardDto targetDto = ActivityBoard.ToDto(domain);
+
+            sourceDto.ShouldBeEquivalentTo(targetDto);
+        }
+
+        [Test]
+        public void DtoAndLiteDbRoundTrip()
+        {
+            Fixture fixture = TestHelper.CreateSerializationAutoFixture(useLiteDBCompatibleDateTime: true);
+
+            ActivityBoardDto sourceDto = fixture.Create<ActivityBoardDto>();
+            ActivityBoardDto targetDto;
+            using (var db = new LiteDatabase(new MemoryStream()))
+            {
+                var col = db.GetCollection<ActivityBoardDto>();
+                var id = col.Insert(sourceDto);
+                targetDto = col.FindById(id);
+            }
+
+            sourceDto.ShouldBeEquivalentTo(targetDto);
+        }
+    }
+}
